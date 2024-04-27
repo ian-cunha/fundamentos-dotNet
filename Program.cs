e7be3497799c8b340627aaf532150fd794c87290
ian-cunha <iancunha20@gmail.com>
2024-04-27 01:17:09 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using fundamentos.Models;
+
+Pessoa pessoa1 = new Pessoa
+{
+    Nome = "Ian",
+    Idade = 24
+};
+
+/* 
+Modelo antigo:
+
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Ian";
+pessoa1.Idade = 24;
+*/
+
+pessoa1.Apresentar();
