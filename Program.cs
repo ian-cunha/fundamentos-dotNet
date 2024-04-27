b4c2ed9f6939688869810678ec55d1672743c5e6
ian-cunha <iancunha20@gmail.com>
2024-04-27 03:12:17 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿// using fundamentos.Models;
 
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual);
+
+/* 
 string apresentacao = "Olá, seja bem vindo!";
 
 int quantidade = 1;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+quantidade = 10;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
 
 double altura = 1.80;
 
@@ -15,6 +23,7 @@ Console.WriteLine("Valor da variável quantidade: " + quantidade);
 Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
 Console.WriteLine("Valor da variável preco: " + preco);
 Console.WriteLine("Valor da variável condicao: " + condicao);
+*/
 
 /* 
 
