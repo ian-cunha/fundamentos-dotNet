1ba841850f22158b43e301b9f97a80b7eec0daf3
ian-cunha <iancunha20@gmail.com>
2024-05-05 19:46:43 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
 ﻿using fundamentos.dotNet.Models;
 
+/*
+// Repeticao WHILE
+int numero = 5;
+int contador = 1;
+
+while (contador <= 10)
+{
+    Console.WriteLine($"{contador}° Execução: {numero} x {contador} = {numero * contador}");
+    contador++;
+
+    if (contador == 6)
+    {
+        break;
+    }
+}
+*/
+
+/*
 // Repetição FOR
 int numero = 5;
 
@@ -7,6 +25,7 @@ for(int contador = 0; contador <= 100; contador++)
 {
     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
 }
+*/
 
 
 //Calculadora calc = new Calculadora();
--- obj/Debug/net8.0/fundamentos.AssemblyInfo.cs
+++ obj/Debug/net8.0/fundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f5974894123b08073443ee29e3cd2f567d1dff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67068a24ee6e46a4da25a5ecc4d3b312b0604637")]
 [assembly: System.Reflection.AssemblyProductAttribute("fundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
