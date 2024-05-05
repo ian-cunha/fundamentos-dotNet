41f5974894123b08073443ee29e3cd2f567d1dff
Ian <iancunha20@gmail.com>
2024-05-05 00:12:13 -0300

    update

--- Program.cs
+++ Program.cs
@@ -3,14 +3,33 @@
 //Calculadora calc = new Calculadora();
 Calculadora calc = new();
 
-calc.Somar(10, 30);
-calc.Subtrair(10, 50);
-calc.Multiplicar(15, 45);
-calc.Dividir(2, 2);
-calc.Potencia(3, 3);
-calc.Seno(30);
-calc.Coseno(30);
-calc.Tangente(30);
+//calc.Somar(10, 30);
+//calc.Subtrair(10, 50);
+//calc.Multiplicar(15, 45);
+//calc.Dividir(2, 2);
+//calc.Potencia(3, 3);
+//calc.Seno(30);
+//calc.Coseno(30);
+//calc.Tangente(30);
+calc.RaizQuadrada(9);
+
+/*
+int numeroIncremento = 10;
+
+Console.WriteLine(numeroIncremento);
+Console.WriteLine("Incrementando o 10");
+//numeroIncremento += 1;
+numeroIncremento++;
+Console.WriteLine(numeroIncremento);
+
+int numeroDecremento = 20;
+
+Console.WriteLine(numeroDecremento);
+Console.WriteLine("Decrementando o 20");
+//numeroIncremento -= 1;
+numeroDecremento--;
+Console.WriteLine(numeroDecremento);
+*/
 
 /*
 // Negação !
