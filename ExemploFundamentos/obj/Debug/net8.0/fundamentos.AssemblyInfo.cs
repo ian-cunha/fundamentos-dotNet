f301fe9f26d590699e5db650cc91a9b1b0ce7b95
ian-cunha <iancunha20@gmail.com>
2024-05-06 21:42:12 -0300

    update

--- ExemploFundamentos/obj/Debug/net8.0/fundamentos.AssemblyInfo.cs
+++ ExemploFundamentos/obj/Debug/net8.0/fundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+502bfa5f333ed76ff9456bdc613b24640cf0e45b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cc1abecc874f031f6925944e3b7e8ea5b9e846b")]
 [assembly: System.Reflection.AssemblyProductAttribute("fundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
